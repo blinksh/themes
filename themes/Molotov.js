2d1731853662b6720f837163e9da612e57f6feb3
Gianni Chiappetta <gianni@runlevel6.org>
2018-04-12 18:28:54 -0500

    Add Molotov theme

---
+++ themes/Molotov.js
@@ -0,0 +1,27 @@
+black       = '#000000'; // black (coal)
+red         = '#ED3A2C'; // red
+green       = '#1ABC9C'; // green (turqoise)
+yellow      = '#F39C12'; // yellow (orange)
+blue        = '#3498DB'; // blue (peter river)
+magenta     = '#E74C3C'; // magenta (alizarin)
+cyan        = '#76E0F3'; // cyan
+white       = '#ECF0F1'; // white (clouds)
+lightBlack  = '#857F78'; // light black (gravel)
+lightRed    = '#D35400'; // light red (pumpkin)
+lightGreen  = '#2ECC71'; // light green (emerald)
+lightYellow = '#F4CF86'; // light yellow (dirtyblonde)
+lightBlue   = '#6871FF'; // light blue
+lightMagenta= '#E74C3C'; // light magenta (alizarin)
+lightCyan   = '#60FDFF'; // light cyan
+lightWhite  = '#FFFFFF'; // white (snow)
+
+t.prefs_.set('color-palette-overrides', [
+  black,      red,      green,      yellow,      blue,      magenta,      cyan,      white,
+  lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite
+]);
+
+t.prefs_.set('cursor-blink', true);
+t.prefs_.set('cursor-color', 'rgba(255, 255, 255, 0.6)');
+
+t.prefs_.set('foreground-color', '#C7C7C7');
+t.prefs_.set('background-color', '#1C1B1A');
