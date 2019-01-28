941760914bc671e27e6939923d733f7fb3e395ce
Adam Tucker <adam.j.tucker@gmail.com>
2019-01-28 17:54:36 -0500

    Add NightOwl theme.

---
+++ themes/NightOwl.js
@@ -0,0 +1,5 @@
+t.prefs_.set('color-palette-overrides',["#011627", "#f78c6c", "#addb67", "#ffcb8b", "#82aaff", "#c792ea", "#7fdbca", "#bec5d4", "#44596b", "#ef5350", "#22da6e", "#ffeb95", "#5ca7e4", "#7e57c2", "#21c7a8", "#ffffff"]);
+t.prefs_.set('foreground-color', "#d6deeb");
+t.prefs_.set('background-color', "#011627");
+t.prefs_.set('cursor-color', 'rgba(111,118,130,0.5)');
+
