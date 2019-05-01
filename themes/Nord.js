62bd4abfbe72c7cf16532f7426016f488821d512
Jamie Dumont <jamiedumont@users.noreply.github.com>
2019-05-01 12:30:16 +0100

    Create Nord.js

---
+++ themes/Nord.js
@@ -0,0 +1,43 @@
+cursor = "#d8dee9";
+foreground = "#d8dee9";
+background = "#2e3440";
+
+color0  = "#3b4252";
+color1  = "#bf616a";
+color2  = "#a3be8c";
+color3  = "#ebcb8b";
+color4  = "#81a1c1";
+color5  = "#b48ead";
+color6  = "#88c0d0";
+color7  = "#e5e9f0";
+color8  = "#4c566a";
+color9  = "#bf616a";
+color10 = "#a3be8c";
+color11 = "#ebcb8b";
+color12 = "#81a1c1";
+color13 = "#b48ead";
+color14 = "#8fbcbb";
+color15 = "#eceff4";
+
+t.prefs_.set('color-palette-overrides', [
+  color0,
+  color1,
+  color2,
+  color3,
+  color4,
+  color5,
+  color6,
+  color7,
+  color8,
+  color9,
+  color10,
+  color11,
+  color12,
+  color13,
+  color14,
+  color15
+]);
+
+t.prefs_.set('cursor-color', cursor);
+t.prefs_.set('foreground-color', foreground);
+t.prefs_.set('background-color', background);
