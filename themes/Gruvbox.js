f0f5082ae0ddb1a33ec9bba14e3604ac6021a104
Adam Tucker <adam.j.tucker@gmail.com>
2019-12-20 20:10:41 -0500

    Add Gruvbox theme.

---
+++ themes/Gruvbox.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#282828", "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#a89984", "#928374", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#ebdbb2"]);
+t.prefs_.set('foreground-color', "#ebdbb2");
+t.prefs_.set('background-color', "#282828");
+t.prefs_.set('cursor-color', 'rgba(235,219,178,0.5)');
