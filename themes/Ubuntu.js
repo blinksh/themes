8e610da03fd527ce68ced6c0d1c678e063c80acd
Carlos Thurber <thurber.carlos@gmail.com>
2020-03-31 18:31:05 -0500

    Add Ubuntu theme

---
+++ themes/Ubuntu.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#36342e", "#0000cc", "#069a4e", "#00a0c4", "#a46534", "#7b5075", "#9a9806", "#cfd7d3", "#535755", "#2929ef", "#34e28a", "#4fe9fc", "#cf9f72", "#a87fad", "#e2e234", "#eceeee"]);
+t.prefs_.set('foreground-color', "#eceeee");
+t.prefs_.set('background-color', "#240a30");
+t.prefs_.set('cursor-color', 'rgba(187,187,187,0.5)');
