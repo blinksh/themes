760a16927fd79864e597f267e3ec98f9045909ab
Houjun Liu <hliu@shabang.cf>
2020-04-23 15:15:31 -0700

    Create ayu.js

---
+++ themes/ayu.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#ff3333", "#b8cc52", "#e7c547", "#36a3d9", "#f07178", "#95e6cb", "#ffffff", "#323232", "#ff6565, "#eafe84", "#fff779", "#68d5ff", "#ffa3aa", "#c7fffd", "#ffffff"]);
+t.prefs_.set('foreground-color', "#e6e1cf");
+t.prefs_.set('background-color', "#0f1419");
+t.prefs_.set('cursor-color', 'rgba(242,151,24,0.5)');
