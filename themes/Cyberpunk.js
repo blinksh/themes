3f7e2a33d4818071a67c4590084e27bc974ac1b6
tmarsh <tmarsh@palantir.com>
2020-05-04 12:55:57 +0100

    Adding Cyberpunk theme

---
+++ themes/Cyberpunk.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000, #ff7092, #00fbac, #fffa6a, #00bfff, #df95ff, #86cbfe, #ffffff, #000000, #ff8aa4, #21f6bc, #fff787, #1bccfd, #e6aefe, #99d6fc,#ffffff"]);
+t.prefs_.set('foreground-color', "#e5e5e5");
+t.prefs_.set('background-color', "#332a57");
+t.prefs_.set('cursor-color', "#21f6bc");
