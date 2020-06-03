98435d5dfa2e2443e2c2f3a9fdce9c3497f314f9
tmarsh <tmarsh@palantir.com>
2020-06-03 18:48:24 +0100

    Adding Dieci clone

---
+++ themes/Dieci.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#f8f8f8", "#000000", "#505050", "#5c2a11", "#d98c98", "#664102", "#644004", "#ffd424", "#f8f8f8", "#000000", "#5c2a11", "#664102", "#644004", "#b27307", "#ffd424", "#f7f7f7"]);
+t.prefs_.set('foreground-color', "#f8f8f8");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
