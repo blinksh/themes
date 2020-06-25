55f85048d07ac6a6f981fa296a3c60d09185da9f
Thanh <quang.thanh.ta@pr1meti.de>
2020-06-25 07:32:32 +0200

    Create One Half Light.js

---
+++ themes/One Half Light.js
@@ -0,0 +1,23 @@
+// Run in the JavaScript console of the hterm browser window
+var colorPaletteOverrides = [
+	"#383a42",  /*  0: black    */
+	"#e45649",  /*  1: red      */
+	"#50a14f",  /*  2: green    */
+	"#c18401",  /*  3: yellow   */
+	"#0184bc",  /*  4: blue     */
+	"#a626a4",  /*  5: magenta  */
+	"#0997b3",  /*  6: cyan     */
+	"#fafafa",  /*  7: white    */
+	"#282c34",  /*  8: brblack  */
+	"#e06c75",  /*  9: brred    */
+	"#98c379",  /* 10: brgreen  */
+	"#e5c07b",  /* 11: bryellow */
+	"#61afef",  /* 12: brblue   */
+	"#c678dd",  /* 13: brmagenta*/
+	"#56b6c2",  /* 14: brcyan   */
+	"#dcdfe4",  /* 15: brwhite  */
+];
+t.prefs_.set('color-palette-overrides',colorPaletteOverrides);
+t.prefs_.set('foreground-color', "#383a42");
+t.prefs_.set('background-color', "#fafafa");
+t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
