05f9e3ff9cf96ee4322752733ef5456a96841ed9
Thanh <quang.thanh.ta@pr1meti.de>
2020-06-25 07:22:17 +0200

    Create One Half Dark.js

---
+++ themes/One Half Dark.js
@@ -0,0 +1,23 @@
+// Run in the JavaScript console of the hterm browser window
+var colorPaletteOverrides = [
+	"#282c34",  /*  0: black    */
+	"#e06c75",  /*  1: red      */
+	"#98c379",  /*  2: green    */
+	"#e5c07b",  /*  3: yellow   */
+	"#61afef",  /*  4: blue     */
+	"#c678dd",  /*  5: magenta  */
+	"#56b6c2",  /*  6: cyan     */
+	"#dcdfe4",  /*  7: white    */
+	"#383a42",  /*  8: brblack  */
+	"#e45649",  /*  9: brred    */
+	"#50a14f",  /* 10: brgreen  */
+	"#c18401",  /* 11: bryellow */
+	"#0184bc",  /* 12: brblue   */
+	"#a626a4",  /* 13: brmagenta*/
+	"#0997b3",  /* 14: brcyan   */
+	"#fafafa",  /* 15: brwhite  */
+];
+t.prefs_.set('color-palette-overrides',colorPaletteOverrides);
+t.prefs_.set('foreground-color', "#dcdfe4");
+t.prefs_.set('background-color', "#282c34");
+t.prefs_.set('cursor-color', 'rgba(74,69,67,0.5)');
