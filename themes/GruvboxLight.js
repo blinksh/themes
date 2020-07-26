d236e98e5795ac596229a877e2598c4a7ac127e4
uajqq <46248396+uajqq@users.noreply.github.com>
2020-07-26 12:42:13 -0400

    Added separate Gruvbox dark and light

---
+++ themes/GruvboxLight.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#fbf1c7", "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#7c6f64", "#928374", "#9d0006", "#79740e", "#b57614", "#076678", "#8f3f71", "#427b58", "#3c3836"]);
+t.prefs_.set('foreground-color', "#3c3836");
+t.prefs_.set('background-color', "#ebdbb2");
+t.prefs_.set('cursor-color', 'rgba(20,36,77,0.5)');
