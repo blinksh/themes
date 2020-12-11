b48d3f12d49b48d42de2d3a34da7cccce97d0975
Wonjun Kim <kimjnsjwj@gmail.com>
2020-12-11 18:29:30 +0900

    Adding Shades of Purple
    from https://github.com/mbadolato/iTerm2-Color-Schemes

---
+++ themes/Shades of Purple.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#d90429", "#3ad900", "#ffe700", "#6943ff", "#ff2c70", "#00c5c7", "#c7c7c7", "#686868", "#f92a1c", "#43d426", "#f1d000", "#6871ff", "#ff77ff", "#79e8fb", "#ffffff"]);
+t.prefs_.set('foreground-color', "#ffffff");
+t.prefs_.set('background-color', "#1e1d40");
+t.prefs_.set('cursor-color', '#fad000');
