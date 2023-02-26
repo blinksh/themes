b5ec541f65b6160fc7959c6a9879ee23ecbf409d
jaro <eichler.jaro@gmail.com>
2023-02-26 20:30:02 +0100

    Add light material amber based theme

---
+++ themes/MaterialAmber.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#21201d", "#cd4340", "#498d49", "#fab32d", "#3378c4", "#b83269", "#21929a", "#ffd7d7", "#66635a", "#dd7b72", "#82ae78", "#fbc870", "#73a0cd", "#ce6f8e", "#548c94", "#ffe1da"]);
+t.prefs_.set('foreground-color', "#21201d");
+t.prefs_.set('background-color', "#fff8e1");
+t.prefs_.set('cursor-color', 'rgba(33,32,29,0.5)');
