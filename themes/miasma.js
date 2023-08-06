7cfb05c4edd9b16d7d061fdf92e47bab314aacf1
xero <x@xero.style>
2023-08-06 02:24:00 -0400

    add miasma

---
+++ themes/miasma.js
@@ -0,0 +1,24 @@
+// ┏┏┓o┳━┓┓━┓┏┏┓┳━┓
+// ┃┃┃┃┃━┫┗━┓┃┃┃┃━┫
+// ┛ ┇┇┛ ┇━━┛┛ ┇┛ ┇
+//  miasma by xero (https://x-e.ro)
+
+black = '#222222'
+altBlack = '#666666'
+red = '#685742'
+green = '#5f875f'
+yellow = '#B36D43'
+blue = '#78824B'
+magenta = '#bb7744'
+cyan = '#C9A554'
+white = '#D7C483'
+
+t.prefs_.set('color-palette-overrides', [
+	black, red, green, yellow,
+	blue, magenta, cyan, white,
+	altBlack, red, green, yellow,
+	blue, magenta, cyan, white
+]);
+t.prefs_.set('cursor-color', 'rgba(105, 168, 80, 0.5)');
+t.prefs_.set('background-color', '#222222');
+t.prefs_.set('foreground-color', '#c2c2b0');
