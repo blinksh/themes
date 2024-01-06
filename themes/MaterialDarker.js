7309a1a4f862dcf69d97e12ed6a7334233091f7e
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-01-07 02:38:07 +0900

    feat: add MaterialDarker

---
+++ themes/MaterialDarker.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#ff5370", "#c3e88d", "#ffcb6b", "#82aaff", "#c792ea", "#89ddff", "#ffffff", "#545454", "#ff5370", "#c3e88d", "#ffcb6b", "#82aaff", "#c792ea", "#89ddff", "#ffffff"]);
+t.prefs_.set('foreground-color', "#eeffff");
+t.prefs_.set('background-color', "#212121");
+t.prefs_.set('cursor-color', 'rgba(255,255,255,0.5)');
