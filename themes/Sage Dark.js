640fb2000d12cb2b7697cfbf9f87977f42e1d90c
YannickHerrero <yannick.herrero@proton.me>
2026-07-14 11:05:50 +0200

    Add Sage Dark and Sage Light themes

---
+++ themes/Sage Dark.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides', ["#2A3024", "#CF6B53", "#8FC07A", "#D7A64C", "#9DBE82", "#9DBE82", "#A6AF96", "#D6DCC7", "#6B7560", "#CF6B53", "#8FC07A", "#D7A64C", "#9DBE82", "#9DBE82", "#A6AF96", "#EDF1E4"]);
+t.prefs_.set('foreground-color', "#D6DCC7");
+t.prefs_.set('background-color', "#161A12");
+t.prefs_.set('cursor-color', 'rgba(157,190,130,0.65)');
